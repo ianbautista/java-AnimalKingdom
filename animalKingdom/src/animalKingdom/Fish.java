dd2125554239af76a2cb9d2031fa407caf5afc4f
ianbautista <ianfbautista@gmail.com>
2020-09-01 14:23:27 -0700

    created fish class

--- animalKingdom/src/animalKingdom/Fish.java
+++ animalKingdom/src/animalKingdom/Fish.java
@@ -2,5 +2,26 @@ package animalKingdom;
 
 public class Fish extends Animal
 {
-	
+	public Fish(String name, int yearDiscovered)
+	{
+		super(name, yearDiscovered);
+	}
+
+	@Override
+	public String move()
+	{
+		return "swim";
+	}
+
+	@Override
+	public String breath()
+	{
+		return "gills";
+	}
+
+	@Override
+	public String reproduce()
+	{
+		return "eggs";
+	}
 }
