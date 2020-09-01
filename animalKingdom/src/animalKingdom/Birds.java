4f28f3bbcfa481e92b365441fd2b2b80d01425c9
ianbautista <ianfbautista@gmail.com>
2020-09-01 14:22:31 -0700

    created birds class

--- animalKingdom/src/animalKingdom/Birds.java
+++ animalKingdom/src/animalKingdom/Birds.java
@@ -2,5 +2,26 @@ package animalKingdom;
 
 public class Birds extends Animal
 {
-	
+	public Birds(String name, int yearDiscovered)
+	{
+		super(name, yearDiscovered);
+	}
+
+	@Override
+	public String move()
+	{
+		return "fly";
+	}
+
+	@Override
+	public String breath()
+	{
+		return "lungs";
+	}
+
+	@Override
+	public String reproduce()
+	{
+		return "eggs";
+	}
 }
