c2c0b742d06a429b8ffd6f0ab5f99e98bf3fda15
ianbautista <ianfbautista@gmail.com>
2020-09-01 15:11:24 -0700

    instantiated all the mammals, birds, and fish

--- animalKingdom/src/animalKingdom/Main.java
+++ animalKingdom/src/animalKingdom/Main.java
@@ -1,9 +1,33 @@
 package animalKingdom;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Main
 {
 	public static void main(String[] args)
 	{
 		System.out.println("Christian's Animal Kingdom!!!");
+
+		//mammals
+		Mammals panda = new Mammals("Panda", 1869);
+	    Mammals zebra = new Mammals("Zebra", 1778);
+	    Mammals koala = new Mammals("Koala", 1816);
+	    Mammals sloth = new Mammals("Sloth", 1804);
+	    Mammals armadillo = new Mammals("Armadillo", 1758);
+	    Mammals raccoon = new Mammals("Raccoon", 1758);
+	    Mammals bigfoot = new Mammals("Bigfoot", 2021);
+
+	    //birds
+	    Birds pigeon = new Birds("Pigeon", 1837);
+	    Birds peacock = new Birds("Peacock", 1821);
+	    Birds toucan = new Birds("Toucan", 1758);
+	    Birds parrot = new Birds("Parrot", 1824);
+	    Birds swan = new Birds("Swan", 1758);
+
+	    //fish
+	    Fish salmon = new Fish("Salmon", 1758);
+    	Fish catfish = new Fish("Catfish", 1817);
+    	Fish perch = new Fish("Perch", 1758);
 	}
 }
